3f99b1a6f0123f43ffa8c9973a63c5b0c08964bf
NicolasStettler <64642250+NicolasStettler@users.noreply.github.com>
2025-04-17 11:25:38 +0200

    Update folder names

---
+++ combine_data.py
@@ -0,0 +1,19 @@
+'''
+This file combines all the data to a single dataframe
+'''
+
+import pandas as pd
+import os
+
+class CSVData:
+    def __init__(self, folder_path):
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".csv"):
+                var_name = os.path.splitext(filename)[0].replace(" ", "_").replace("-", "_")
+                df = pd.read_csv(os.path.join(folder_path, filename))
+                setattr(self, var_name, df)
+
+# Usage
+folder_path = "data/Folder_1"
+data = CSVData(folder_path)
+
