38aa0d17f1f60f39c9f3d055c7254b4e0c5b1c55
NicolasStettler <64642250+NicolasStettler@users.noreply.github.com>
2025-04-17 15:16:43 +0200

    Update to data extraction.

--- combine_data.py
+++ combine_data.py
@@ -15,7 +15,72 @@ class CSVData:
 
 # Usage
 folder_path = "data/Folder_1/dataset"
-data = CSVData(folder_path)
+data_1 = CSVData(folder_path)
 
-print(data.dailyActivity_merged)
+folder_path = "data/Folder_1/dataset"
+data_2 = CSVData(folder_path)
 
+
+class MergedData:
+    def __init__(self, data_1, data_2):
+        for attr in dir(data_1):
+            if not attr.startswith("_") and isinstance(getattr(data_1, attr), pd.DataFrame):
+                df1 = getattr(data_1, attr)
+                df2 = getattr(data_2, attr, None)
+                if isinstance(df2, pd.DataFrame):
+                    merged_df = pd.concat([df1, df2], ignore_index=True)
+                else:
+                    merged_df = df1
+                setattr(self, attr, merged_df)
+
+merged_data = MergedData(data_1, data_2)
+
+
+'''
+
+Here we merge all data that is stored by every minute.
+
+'''
+
+merged_data.minuteCaloriesNarrow_merged = merged_data.minuteCaloriesNarrow_merged.drop_duplicates(subset=["Id", "ActivityMinute"])
+merged_data.minuteIntensitiesNarrow_merged = merged_data.minuteIntensitiesNarrow_merged.drop_duplicates(subset=["Id", "ActivityMinute"])
+merged_data.minuteMETsNarrow_merged = merged_data.minuteMETsNarrow_merged.drop_duplicates(subset=["Id", "ActivityMinute"])
+merged_data.minuteStepsNarrow_merged = merged_data.minuteStepsNarrow_merged.drop_duplicates(subset=["Id", "ActivityMinute"])
+merged_data.minuteSleep_merged = merged_data.minuteSleep_merged.drop_duplicates(subset=["Id", "date"])
+
+
+merged_data.minutes_df = pd.merge(merged_data.minuteCaloriesNarrow_merged, merged_data.minuteIntensitiesNarrow_merged, on=["Id", "ActivityMinute"], how="outer")
+merged_data.minutes_df = pd.merge(merged_data.minutes_df, merged_data.minuteMETsNarrow_merged, on=["Id", "ActivityMinute"], how="outer")
+merged_data.minutes_df = pd.merge(merged_data.minutes_df, merged_data.minuteStepsNarrow_merged, on=["Id", "ActivityMinute"], how="outer")
+merged_data.minutes_df = pd.merge(merged_data.minutes_df, merged_data.minuteSleep_merged, left_on=["Id", "ActivityMinute"], right_on=["Id", "date"], how="outer")
+
+# Update ActivityMinute with values from date where ActivityMinute is missing
+merged_data.minutes_df["ActivityMinute"] = merged_data.minutes_df["ActivityMinute"].combine_first(merged_data.minutes_df["date"])
+
+# Drop the date column
+merged_data.minutes_df = merged_data.minutes_df.drop(columns=["date"])
+
+
+merged_data.minutes_df["ActivityMinute"] = pd.to_datetime(merged_data.minutes_df["ActivityMinute"], format="%m/%d/%Y %I:%M:%S %p")
+
+print(merged_data.minutes_df)
+
+
+'''
+
+Here we merge all data that is stored hourly.
+
+'''
+
+
+
+merged_data.hourlyCalories_merged = merged_data.hourlyCalories_merged.drop_duplicates(subset=["Id", "ActivityHour"])
+merged_data.hourlyIntensities_merged = merged_data.hourlyIntensities_merged.drop_duplicates(subset=["Id", "ActivityHour"])
+merged_data.hourlySteps_merged = merged_data.hourlySteps_merged.drop_duplicates(subset=["Id", "ActivityHour"])
+
+
+merged_data.hourly_df = pd.merge(merged_data.hourlyCalories_merged, merged_data.hourlyIntensities_merged, on=["Id", "ActivityHour"], how="inner")
+merged_data.hourly_df = pd.merge(merged_data.hourly_df, merged_data.hourlySteps_merged, on=["Id", "ActivityHour"], how="inner")
+
+merged_data.hourly_df["ActivityHour"] = pd.to_datetime(merged_data.hourly_df["ActivityHour"], format="%m/%d/%Y %I:%M:%S %p")
+print(merged_data.hourly_df)
